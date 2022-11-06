b2d43fdaac94ba4080d982de40484a9b08d115c6
unknown <nguyenthanhlongffse2001@gmail.com>
2022-11-06 13:40:19 +0700

    ...

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
-import style from './Home.module.scss';
-import {DesktopLogo} from '~/components/Icons'
+import styles from './Home.module.scss';
+import { DesktopLogo } from '~/components/Icons';
+import Button from '~/components/UI/Button';
 
-const cx = classNames.bind(style);
+const cx = classNames.bind(styles);
 
 const Home = (props) => {
+    const handleOnclickDemo = () => {};
+    const handleOnclickGetStart = () => {};
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('title-home-page')}>
-                <h1>REACT FUNCTION COMPONENTS</h1>
+                <h1 className={cx('title')}>REACT FUNCTION COMPONENTS</h1>
+                <span className={cx('desciption')}>
+                    Performant, flexible and extensible forms with easy-to-use
+                    validation.
+                </span>
+            </div>
+            <div className={cx('button-module')}>
+                <Button
+                    className={cx('button')}
+                    onclick={handleOnclickDemo}
+                    primary
+                >
+                    Demo
+                </Button>
+                <Button
+                    className={cx('button')}
+                    onclick={handleOnclickGetStart}
+                    primary
+                >
+                    Get Started
+                </Button>
             </div>
         </div>
     );
